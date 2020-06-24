2f39d19c128ea12d1c1eb0f65c5df83a4545e70a
DannyMcISaac <danielmcisaac@uwaterloo.ca>
2020-06-24 11:43:29 -0400

    practice ANOVA

---
+++ SLA_ANOVA.R
@@ -0,0 +1,19 @@
+SLA <- read.csv("Data/SLA_Canadian cities.csv")
+SLA
+
+library(vegan)
+
+library(car)
+library(tidyverse)
+
+glimpse(SLA)
+
+unique(SLA$City)
+
+histogram <- ggplot(SLA, aes(x=value,Fill=City)) + facet_wrap(~City)+ geom_histogram(bins = 50)
+histogram
+
+ANOVA <- lm (value~City, data = SLA)
+summary(ANOVA)
+
+Anova(ANOVA, type = "2")
